6a5982e72f2d5bed17970ff02045d507e60ef981
sheng yeong <chia.shengyeong@gmail.com>
2022-08-27 16:39:05 +0800

    try catch errors from api call with mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,16 @@
 import { MongoClient } from "mongodb";
 
+
+async function connectDatabase() {
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("emails").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -9,10 +20,22 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(process.env.MONGO_URL)
-    const db = client.db();
-    await db.collection("emails").insertOne({email: userEmail});
-    client.close();
+    let client;
+
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({message: "Failed to connect to the database"});
+      return;
+    }
+
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({message: "Insert data failed"});
+      return;      
+    }
 
     res.status(201).json({message: "Signed up!"});
   }
