caa2eb7324ff9d398a73aff90dec745fb01ba1b7
sheng yeong <chia.shengyeong@gmail.com>
2022-08-29 09:48:10 +0800

    demonstrate use context with notifications

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,26 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 
 import MainHeader from './main-header';
+import Notification from "../ui/notification";
+import NotificationContext from '../../store/notification-context';
 
 function Layout(props) {
+  // call and use notification context 
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification &&
+        <Notification
+          title= {activeNotification.title}
+          message= {activeNotification.message}
+          status= {activeNotification.status}
+        />
+      }
     </Fragment>
   );
 }
--- store/notification-context.js
+++ store/notification-context.js
@@ -1,15 +1,31 @@
 import { createContext, useState } from "react";
 
 const NotificationContext = createContext({
-  notification: null,
-  showNotification: function() {},
+  notification: null, // { title, message, status }
+  showNotification: function(notificationData) {},
   hideNotification: function() {},
 });
 
 export function NotificationContextProvider(props) {
-  
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = { 
+    notification: activeNotification, 
+    showNotification: showNotificationHandler, 
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
+      {/* allows you to call notification context function from any components encapsulated by this notifications context provider */}
       {props.children}
     </NotificationContext.Provider>
   )
