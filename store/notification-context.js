8a170d58155d3a6a1943d1598f85b8810736f7f1
sheng yeong <chia.shengyeong@gmail.com>
2022-08-29 09:35:13 +0800

    create NotificationContext for notification

---
+++ store/notification-context.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+const NotificationContext = createContext({
+  notification: null,
+  showNotification: function() {},
+  hideNotification: function() {},
+});
+
+export function NotificationContextProvider(props) {
+  
+  return (
+    <NotificationContext.Provider>
+      {props.children}
+    </NotificationContext.Provider>
+  )
+}
+
+export default NotificationContext;
